b14589d812150f04c3fd111c91740f0b7ddacaac
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2025-11-27 14:20:45 +0100

    Update TaskManager.java

--- backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/progress/TaskManager.java
+++ backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/progress/TaskManager.java
@@ -34,8 +34,8 @@ public class TaskManager {
     progressMonitorsByTaskId.put(taskId.toString(), progressMonitor);
     try {
       System.out.println("beep");
-      task.run(progressMonitor);
       System.out.println("I'm a fancy robot fixing issues.");
+      task.run(progressMonitor);
       System.out.println("beep");
     } finally {
       progressMonitor.complete();
